6dea78c0d655fb808d3e59c7130398e9156782b4
Akile Oday <akile.oday@emu.edu.tr>
2021-04-25 00:44:47 +0300

    24.4.2021

--- oopIntro/src/oopIntro/ProductManager.java
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -2,6 +2,7 @@ package oopIntro;
 //is yapan sinifin metodunu yazma kismi
 public class ProductManager {
 	public void addToCart(Product product) {
+		
 		System.out.println("sepete eklendi "+product.name);
 	}
 
